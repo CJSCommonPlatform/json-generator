50ac121a77a9c03bc3f24376dedc47471c531250
Mohamed Farouk <mohamed.farouk@hmcts.net>
2017-01-05 17:21:28 +0000

    validating parser started

---
+++ src/test/java/uk/gov/justice/json/generation/schema/ValidatingJsonSchemaParserTest.java
@@ -0,0 +1,65 @@
+package uk.gov.justice.json.generation.schema;
+
+import org.apache.commons.io.FileUtils;
+import org.everit.json.schema.ValidationException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ValidatingJsonSchemaParserTest {
+
+    @Mock
+    JsonSchemaParser jsonSchemaParser;
+
+    @Mock
+    Validator validator;
+
+    @InjectMocks
+    ValidatingJsonSchemaParser validatingJsonSchemaParser;
+
+    @Test
+    public void shouldValidateAndParseAValidSchema() throws Exception {
+        final String schema = loadAsString("/simple-property-schema.json");
+
+        validatingJsonSchemaParser.validateAndParse(schema);
+
+        verify(validator).validate(schema);
+        verify(jsonSchemaParser).parse(schema);
+    }
+
+
+    @Test
+    public void shouldFailOnValidatingOnIncorrectSchema() throws Exception{
+//        final String schema = loadAsString("/invalid-schema.json");
+//
+//        try {
+//            validator.validate(schema);
+//            fail();
+//        } catch (ValidationException expected) {
+//            final List<ValidationException> causingExceptions = expected.getCausingExceptions();
+//            assertThat(causingExceptions, hasSize(2));
+//            verifyNoMoreInteractions(jsonSchemaParser);
+//        }
+    }
+
+    private String loadAsString(final String schemaPath) throws URISyntaxException, IOException {
+        final File file = new File(Validator.class.getResource(schemaPath).toURI());
+        return FileUtils.readFileToString(file);
+    }
+}
