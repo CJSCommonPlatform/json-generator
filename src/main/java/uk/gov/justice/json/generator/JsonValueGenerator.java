614aee4d7c179162684486193eee2a6fc4efa5db
Justin Cooke <jrrcooke@gmail.com>
2017-01-11 10:18:31 +0000

    Add random decision for non-required feilds

--- src/main/java/uk/gov/justice/json/generator/JsonValueGenerator.java
+++ src/main/java/uk/gov/justice/json/generator/JsonValueGenerator.java
@@ -2,9 +2,6 @@ package uk.gov.justice.json.generator;
 
 import javax.json.JsonValue;
 
-/**
- * Created by david on 10/01/17.
- */
 public interface JsonValueGenerator {
 
     JsonValue nextValue();
--- src/main/java/uk/gov/justice/json/generator/value/EmailGenerator.java
+++ src/main/java/uk/gov/justice/json/generator/value/EmailGenerator.java
@@ -3,11 +3,15 @@ package uk.gov.justice.json.generator.value;
 import uk.gov.justice.services.test.utils.core.random.EmailAddressGenerator;
 
 public class EmailGenerator implements StringGenerator{
-    private EmailAddressGenerator emailAddressGenerator = new EmailAddressGenerator();
+
+    private EmailAddressGenerator emailAddressGenerator;
+
+    public EmailGenerator() {
+        emailAddressGenerator = new EmailAddressGenerator();
+    }
 
     @Override
     public String nextValue() {
         return emailAddressGenerator.next();
     }
-
 }
