3ffa4b32176368ed27d6e8a2122b28a46061648c
David Edwards <david@more.fool.me.uk>
2017-01-13 17:01:12 +0000

    Add basic support for tuple arrays with additional items

--- src/test/java/uk/gov/justice/json/generator/JsonArrayGeneratorTest.java
+++ src/test/java/uk/gov/justice/json/generator/JsonArrayGeneratorTest.java
@@ -1,7 +1,7 @@
 package uk.gov.justice.json.generator;
 
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static javax.json.JsonValue.ValueType.NUMBER;
+import static javax.json.JsonValue.ValueType.STRING;
 import static uk.gov.justice.json.generator.JsonArrayGenerator.DEFAULT_MAX_ITEMS;
 import static uk.gov.justice.json.generator.JsonArrayGenerator.DEFAULT_MIN_ITEMS;
 import static uk.gov.justice.services.test.utils.core.helper.TypeCheck.Times.times;
@@ -10,6 +10,7 @@ import static uk.gov.justice.services.test.utils.core.helper.TypeCheck.typeCheck
 import uk.gov.justice.json.JsonSchemaLoader;
 
 import javax.json.JsonArray;
+import javax.json.JsonValue;
 
 import org.everit.json.schema.ArraySchema;
 import org.junit.Test;
@@ -22,11 +23,36 @@ public class JsonArrayGeneratorTest {
     private static final int NUMBER_OF_TIMES = 1000;
 
     @Test
+    public void shouldGenerateStringsArrays() {
+        final ArraySchema schema = JsonSchemaLoader.loadArraySchema("src/test/resources/schemas/array/strings-schema.json");
+        final JsonArrayGenerator generator = new JsonArrayGenerator(schema);
+
+        typeCheck(generator, this::isAllStrings)
+                .verify(times(NUMBER_OF_TIMES));
+    }
+
+    @Test
+    public void shouldGenerateTupleArrays() {
+        final ArraySchema schema = JsonSchemaLoader.loadArraySchema("src/test/resources/schemas/array/tuple-schema.json");
+        final JsonArrayGenerator generator = new JsonArrayGenerator(schema);
+
+        typeCheck(generator, array -> {
+            if (array.size() > 0 && !NUMBER.equals(array.get(0).getValueType())) {
+                return false;
+            }
+            if (array.size() > 1 && !STRING.equals(array.get(1).getValueType())) {
+                return false;
+            }
+            return true;
+        }).verify(times(NUMBER_OF_TIMES));
+    }
+
+    @Test
     public void shouldGenerateUnboundedArrays() {
         final ArraySchema schema = JsonSchemaLoader.loadArraySchema("src/test/resources/schemas/array/strings-schema.json");
         final JsonArrayGenerator generator = new JsonArrayGenerator(schema);
 
-        typeCheck(generator, array -> array.size() >= DEFAULT_MIN_ITEMS & array.size() <= DEFAULT_MAX_ITEMS)
+        typeCheck(generator, array -> array.size() >= DEFAULT_MIN_ITEMS && array.size() <= DEFAULT_MAX_ITEMS)
                 .verify(times(NUMBER_OF_TIMES));
     }
 
@@ -35,7 +61,16 @@ public class JsonArrayGeneratorTest {
         final ArraySchema schema = JsonSchemaLoader.loadArraySchema("src/test/resources/schemas/array/bounded-strings-schema.json");
         final JsonArrayGenerator generator = new JsonArrayGenerator(schema);
 
-        typeCheck(generator, array -> array.size() >= 3 & array.size() <= 37)
+        typeCheck(generator, array -> array.size() >= 3 && array.size() <= 7)
                 .verify(times(NUMBER_OF_TIMES));
+    }
+
+    private boolean isAllStrings(JsonArray array) {
+        for(JsonValue value : array.getValuesAs(JsonValue.class)) {
+            if (!STRING.equals(value.getValueType())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
