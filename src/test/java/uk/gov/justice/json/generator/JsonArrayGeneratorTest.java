603053027dc6698a5538966616b7dd941b5aec56
David Edwards <david@more.fool.me.uk>
2017-01-13 12:12:28 +0000

    Add tests for bounded and unbounded arrays

--- src/test/java/uk/gov/justice/json/generator/JsonArrayGeneratorTest.java
+++ src/test/java/uk/gov/justice/json/generator/JsonArrayGeneratorTest.java
@@ -2,6 +2,10 @@ package uk.gov.justice.json.generator;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static uk.gov.justice.json.generator.JsonArrayGenerator.DEFAULT_MAX_ITEMS;
+import static uk.gov.justice.json.generator.JsonArrayGenerator.DEFAULT_MIN_ITEMS;
+import static uk.gov.justice.services.test.utils.core.helper.TypeCheck.Times.times;
+import static uk.gov.justice.services.test.utils.core.helper.TypeCheck.typeCheck;
 
 import uk.gov.justice.json.JsonSchemaLoader;
 
@@ -11,15 +15,27 @@ import org.everit.json.schema.ArraySchema;
 import org.junit.Test;
 
 /**
- * Created by david on 12/01/17.
+ * Unit tests for the {@link JsonArrayGenerator} class.
  */
 public class JsonArrayGeneratorTest {
 
+    private static final int NUMBER_OF_TIMES = 1000;
+
     @Test
-    public void shouldGenerateSimpleStringArrays() {
+    public void shouldGenerateUnboundedArrays() {
         final ArraySchema schema = JsonSchemaLoader.loadArraySchema("src/test/resources/schemas/array/strings-schema.json");
-        JsonArrayGenerator generator = new JsonArrayGenerator(schema);
-        JsonArray array = generator.next();
-        assertThat(array.get(0), notNullValue());
+        final JsonArrayGenerator generator = new JsonArrayGenerator(schema);
+
+        typeCheck(generator, array -> array.size() >= DEFAULT_MIN_ITEMS & array.size() <= DEFAULT_MAX_ITEMS)
+                .verify(times(NUMBER_OF_TIMES));
+    }
+
+    @Test
+    public void shouldGenerateBoundedArrays() {
+        final ArraySchema schema = JsonSchemaLoader.loadArraySchema("src/test/resources/schemas/array/bounded-strings-schema.json");
+        final JsonArrayGenerator generator = new JsonArrayGenerator(schema);
+
+        typeCheck(generator, array -> array.size() >= 3 & array.size() <= 37)
+                .verify(times(NUMBER_OF_TIMES));
     }
 }
